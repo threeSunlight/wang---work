2a038ce42a18e465f061e4a2bce3e9feb34deb2b
wbj <709965309@qq.com>
2022-07-22 20:46:42 +0800

    feat: 添加ts文件

--- todoList/todoList.ts
+++ todoList/todoList.ts
@@ -9,70 +9,200 @@ interface ArrayItem {
     value: string
 }
 
-type TodoListArray =  Array<ArrayItem>
+type TodoListArray = Array<ArrayItem>
 
 
 /**获取ul标签 */
-let ul1: HTMLUListElement = document.getElementsByTagName("ul")[0];
+let ul: HTMLUListElement = document.getElementsByTagName("ul")[0];
+//获取底部按钮
+let completed_order: HTMLElement | null = document.getElementById("completed_order")
+let allinfo: HTMLElement | null = document.getElementById("allinfo")
+let completed_todo: HTMLElement | null = document.getElementById("completed_todo")
+let clear_completed: HTMLElement | null = document.getElementById("clear_completed")
 
 /**定义新增数组 */
-let todoArray1: TodoListArray = [];
+let todoArray: TodoListArray = [];
 
 
 /**定义数组自增ID */
-let nextId1: number = 1
-
-
-
-
+let nextId: number = 1
 
 
 /**
- * 新增的方法
+ * 获取新增数据内容
  */
-function addTodoItem(e: { target: { value: string; }; }): void {
-  /**获取新增的内容 */
-  let inputValue: string =  e.target.value 
-  if(todoArray1.length > 0) {
-    nextId1++
-  } 
-    let addItemValue: ArrayItem = {
-        checked: false,
-        id: nextId1,
-        value: inputValue
+function addTodoList(event: { keyCode: number; }) {
+    if (!event || event.keyCode === 13) {
+        // 首先获取input框的输入内容
+        //@ts-ignore
+        let inputValue: string = document.getElementsByClassName("input-todoList")[0].value;
+        if (inputValue) {
+            circulation(inputValue);
+             //@ts-ignore
+            document.getElementsByClassName("input-todoList")[0].value = ''
+        }
     }
-    todoArray1.push(addItemValue)
-    // 调用渲染方法
-    renderHtml(todoArray1)
 }
 
+/**
+ * 新增数据,并且保证数据id的唯一性
+ */
+function circulation(inputValue: string) {
+    // 首先获取ul标签
+    // 存储数据,li标签的循环
+    if (todoArray.length > 0) {
+        nextId++
+    }
+    let addItemValue: ArrayItem = {
+        checked: false,
+        id: nextId,
+        value: inputValue
+    }
+    todoArray.push(addItemValue)
+    circulationHtml(todoArray);
+}
+
+
+/**
+ * 根据数据的变化进行生成html
+ */
+function circulationHtml(date: TodoListArray, checkId?: number) {
+    // 清空原来的元素
+    ul.innerHTML = ''
+    date.forEach((item) => {
+        // 创建li标签
+        let li: HTMLLIElement = document.createElement("li");
+        
+        if(item.id == checkId && item.checked) {
+            li.className = 'completed'
+        } else if(item.checked) {
+            li.className = 'need-completed'
+        } else {
+            li.className = ''
+        }
+        li.id = `${item.id}`
+       
+        if(item.id == checkId && item.checked) {}
+        li.innerHTML = ` 
+          <div class="li-style-div">
+              <input type="checkbox" class="checkbox" id=${item.id} ${item.checked ? "checked" : null
+            } onchange="changeCheckboxDate(this)"></input>
+              <p class="li-style-div-content">${item.value}</p>
+          </div>
+          <button class= ${item.id == checkId && item.checked ? ' completed ' : item.checked ? "need-completed button-close " : "button-close"}  id=${item.id}  onclick=deleteDate(this)>X</button>
+        `;
+        ul.append(li)
+    });
+    changeLeftItem(date)
+}
 
 /**
  * 删除
  */
-function delTodoItem(id: number) {
-   let result: ArrayItem[] =  todoArray1.filter(item => item.id !== id)
-   // 调用渲染方法
-   renderHtml(result)
+function deleteDate(ele: ArrayItem) {
+    todoArray = todoArray.filter(item => item.id != ele.id)
+    circulationHtml(todoArray)
 }
 
 /**
- * 勾选
- * 修改原数据,因为本来数据就变化了,不需要在进行保留
+ * 选中的时候改变数据结构,并且添加样式
  */
-function inputChecked(id:number) {
-    todoArray1.forEach( item => {
-        if(item.id === id) {
+function changeCheckboxDate(ele: ArrayItem) {
+    todoArray.forEach(item => {
+        if (item.id == ele.id) {
             item.checked = !item.checked
         }
     })
-    // 调用渲染方法
-    renderHtml(todoArray1)
+    circulationHtml(todoArray, ele.id)
+}
+
+
+/**
+ * 左下角条数的变化
+ */
+function changeLeftItem(data: TodoListArray) {
+    let docuemnt: HTMLElement | null = document.getElementById('item-current')
+    if (docuemnt) {
+        docuemnt.innerText = data.length <= 0 ? '无事项' : data.length + '事项'
+    }
 }
 
 /**
- * 渲染html方法
+ * 待完成的事项
+ * 获取待办事件,并且更新视图
  */
-function renderHtml(array: TodoListArray) {
+function toBeCompleted() {
+    let result: ArrayItem[] = todoArray.filter(item => !item.checked)
+    circulationHtml(result)
+    footerButton('completed_todo')
+}
 
+/**
+ * 已完成
+ */
+
+function completed() {
+    let result: ArrayItem[] = todoArray.filter((item: ArrayItem) => item.checked)
+    circulationHtml(result)
+    footerButton('completed_order')
+}
+
+/**
+ * 清空所有
+ */
+
+function clearAll() {
+    todoArray = []
+    ul.innerHTML = ''
+    footerButton('clear_completed')
+}
+
+/**
+ * 查看所有
+ */
+function viewAllInfo() {
+    circulationHtml(todoArray)
+    footerButton('allinfo')
+}
+
+
+/**
+ * 定义底部按钮接口类型
+ */
+interface ButtonArry {
+    name: HTMLElement | null,
+    key: string
+}
+/**
+ * 底部元素新增样式
+ */
+
+function footerButton(className: string): void {
+    const buttonArry: Array<ButtonArry> = [
+        {
+            name: completed_order,
+            key: "completed_order"
+        },
+        {
+            name: allinfo,
+            key: "allinfo"
+        },
+        {
+            name: completed_todo,
+            key: "completed_todo"
+        },
+        {
+            name: clear_completed,
+            key: "clear_completed"
+        },
+    ]
+    buttonArry.forEach(item => {
+        if (item && item.key === className) {
+             //@ts-ignore
+           item.name.classList.add('clickButton')
+        } else {
+             //@ts-ignore
+            item.name.classList.remove('clickButton')
+        }
+    })
 }
